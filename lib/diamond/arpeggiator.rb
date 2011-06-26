f436aaa99a47cf6b2c7ce018324f1e3cb72df498
Ari Russo <ari.russo@gmail.com>
2011-06-26 03:37:41 -0400

    midi output

--- lib/diamond/arpeggiator.rb
+++ lib/diamond/arpeggiator.rb
@@ -15,13 +15,37 @@ module Diamond
       resolution = options[:resolution] || 128
       quarter_note = resolution / 4
       
+      initialize_midi_io(options[:midi]) unless options[:midi].nil?
+      
       @sequencer = Sequencer.new(resolution, options)
+      initialize_clock(tempo, resolution)
+      bind_events(&block)
+    end
+    
+    private
+    
+    def initialize_clock(tempo, resolution)
       @clock = Topaz::Tempo.new(tempo)
       dif = resolution / @clock.interval  
       @clock.interval = @clock.interval * dif
-      @clock.on_tick { @sequencer.with_next { |msgs| yield(msgs) } }  
     end
-          
+    
+    def initialize_midi_io(devices)
+      devices = [devices].flatten
+      @midi_destinations = devices.find_all { |d| d.type == :output }
+      @midi_sources = devices.find_all { |d| d.type == :input }   
+    end
+    
+    def bind_events(&block)
+      @clock.on_tick do 
+        @sequencer.with_next do |msgs|
+          data = msgs.map { |msg| msg.to_bytes }.flatten
+          @midi_destinations.each { |o| o.puts(data) } unless data.empty?
+          yield(msgs) unless block.nil?
+        end
+      end 
+    end
+  
   end
   
 end
