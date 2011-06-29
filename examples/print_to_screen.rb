6071eb973f643a4b695e9da85dca2e17f249c35c
Ari Russo <ari.russo@gmail.com>
2011-06-28 22:15:48 -0400

    print to screen example

---
+++ examples/print_to_screen.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+$:.unshift File.join( File.dirname( __FILE__ ), '../lib')
+
+#
+# this is the most basic setup possible
+# we just set up an arpeggiator and let it run in the foreground
+# the messages produced will be printed to the screen
+#
+
+require "diamond"
+require "pp"
+
+@output = UniMIDI::Output.first.open
+
+opts = { 
+  :gate => 90,   
+  :interval => 7,
+  :midi => @output,
+  :pattern => Diamond::Pattern["UpDown"],
+  :range => 4, 
+  :rate => 8
+}
+
+arp = Diamond::Arpeggiator.new(175, opts) do |msgs|
+  pp msgs
+end
+
+include MIDIMessage
+
+notes = [
+  NoteOn["C4"].new(0, 100),
+  NoteOn["E4"].new(0, 100),
+  NoteOn["G4"].new(0, 100)
+]
+
+arp.add(notes)
+   
+arp.start
