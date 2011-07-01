e12bd4183ad22e1cbd8603595fd5461c326f30a2
Ari Russo <ari.russo@gmail.com>
2011-06-30 22:50:31 -0400

    midi clock madness

--- examples/output_midi_clock.rb
+++ examples/output_midi_clock.rb
@@ -2,33 +2,27 @@
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 #
-# this example is the same as the "simple.rb" one, except that it syncs to MIDI clock
-# from a unimidi input
-#
-# we just set up an arpeggiator and let it run in the foreground
-#
+# this is an example where the arpeggiator outputs midi clock
 #
 
 require "diamond"
 
-@input = UniMIDI::Input.first.open
 @output = UniMIDI::Output.first.open
 
 opts = { 
   :gate => 90,   
   :interval => 7,
   :midi => @output,
+  :midi_clock_output => true,
   :pattern => Diamond::Pattern["UpDown"],
   :range => 4, 
   :rate => 8
 }
 
-arp = Diamond::Arpeggiator.new(@input, opts)
+arp = Diamond::Arpeggiator.new(138, opts)
 
 chord = ["C3", "G3", "Bb3", "A4"]
 
 arp.add(chord)
    
 arp.start
-
-# the arpeggiator will actually start when it begins receiving clock messages
