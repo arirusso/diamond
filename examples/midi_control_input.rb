e123542177d823d89034e63d1f24a98957b0772d
Ari Russo <ari.russo@gmail.com>
2011-10-04 00:56:12 -0400

    new cc map

--- examples/midi_control_input.rb
+++ examples/midi_control_input.rb
@@ -8,23 +8,25 @@ $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
 require "diamond"
 
+@input = UniMIDI::Input.gets
 @output = UniMIDI::Output.gets
 
 map = [
   { 
-    :class => MIDIMessage::ControlChange,
-    :index => 0,
-    :input_range => (0..127),
-    :range => (-24..24),
-    :property => :interval,
-    :value => :value
+    :match => {
+      :class => MIDIMessage::ControlChange,
+      :index => 1 }, 
+    :using => :value,
+    :original_range => (0..127),
+    :new_range => (-24..24),
+    :property => :interval
   }
 ]
 
 opts = { 
   :gate => 90,   
   :interval => 7,
-  :midi => @output,
+  :midi => [@input, @output],
   :pattern => Diamond::Pattern["UpDown"],
   :range => 4, 
   :rate => 8,
@@ -32,8 +34,6 @@ opts = {
 }
 
 arp = Diamond::Arpeggiator.new(110, opts)
-
-
 
 chord = ["C3", "G3", "Bb3", "A4"]
 
