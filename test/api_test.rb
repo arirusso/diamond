dee7026b24d64866523ccde3f719b7ef93d5a10c
Ari Russo <ari.russo@gmail.com>
2014-09-17 15:40:02 -0700

    better test structure

---
+++ test/api_test.rb
@@ -0,0 +1,148 @@
+require "helper"
+
+class Diamond::APITest < Test::Unit::TestCase
+
+  context "API" do
+
+    context "MIDI" do
+
+      context "#omni_on" do
+
+        setup do
+          @messages = [
+            MIDIMessage::NoteOn["C4"].new(10, 100),
+            MIDIMessage::NoteOn["C4"].new(3, 100),
+            MIDIMessage::NoteOn["C4"].new(2, 100)
+          ]
+          @arpeggiator = Diamond::Arpeggiator.new(:rx_channel => 3)
+        end
+
+        should "set omni on" do
+          @arpeggiator.add(@messages[0])
+          assert_empty @arpeggiator.sequence.instance_variable_get("@input_queue")
+
+          @arpeggiator.add(@messages[1])
+          @arpeggiator.sequence.instance_variable_get("@input_queue").expects(:concat).once.with([@messages[1]])
+
+          @arpeggiator.omni_on
+          @arpeggiator.add(@messages[2])
+          @arpeggiator.sequence.instance_variable_get("@input_queue").expects(:concat).once.with([@messages[2]]) 
+        end
+
+      end
+
+      context "#add_midi_source" do
+
+        setup do
+          @input = $test_device[:input]
+          @arpeggiator = Diamond::Arpeggiator.new
+          refute @arpeggiator.midi_sources.include?(@input)
+        end
+
+        should "add a midi source" do
+          @arpeggiator.add_midi_source(@input)
+          assert_not_empty @arpeggiator.midi_sources
+          assert @arpeggiator.midi_sources.include?(@input)
+        end
+
+      end
+
+      context "#remove_midi_source" do
+
+        setup do
+          @input = $test_device[:input]
+          @arpeggiator = Diamond::Arpeggiator.new
+          @arpeggiator.add_midi_source(@input)
+          assert_not_empty @arpeggiator.midi_sources
+          assert @arpeggiator.midi_sources.include?(@input)
+        end
+
+        should "remove a midi source" do
+          @arpeggiator.remove_midi_source(@input)
+          refute @arpeggiator.midi_sources.include?(@input)
+        end
+
+      end
+
+      context "#mute" do
+
+        setup do
+          @arpeggiator = Diamond::Arpeggiator.new
+        end
+
+        should "mute the arpeggiator" do
+          refute @arpeggiator.muted?     
+          @arpeggiator.mute = true
+          assert @arpeggiator.muted?
+          @arpeggiator.mute = false
+          refute @arpeggiator.muted?
+          @arpeggiator.toggle_mute
+          assert @arpeggiator.muted?
+        end
+
+      end
+
+    end
+
+    context "SequenceParameters" do
+
+      setup do
+        @arpeggiator = Diamond::Arpeggiator.new
+      end
+
+      context "#rate=" do
+
+        should "set the rate" do
+          assert_not_equal 16, @arpeggiator.rate
+          @arpeggiator.rate = 16
+          assert_equal 16, @arpeggiator.rate
+        end
+
+      end
+
+      context "#range=" do
+
+        should "set the range" do
+          @arpeggiator.range = 4
+          assert_equal 4, @arpeggiator.range
+          @arpeggiator.range += 1
+          assert_equal 5, @arpeggiator.range 
+        end
+
+      end
+
+      context "#interval=" do
+
+        should "set the interval" do
+          assert_not_equal 7, @arpeggiator.interval
+          @arpeggiator.interval = 7
+          assert_equal 7, @arpeggiator.interval 
+        end
+
+      end
+
+      context "#gate=" do
+
+        should "set the gate" do
+          assert_not_equal 125, @arpeggiator.gate
+          @arpeggiator.gate = 125
+          assert_equal 125, @arpeggiator.gate 
+        end
+
+      end
+
+      context "#pattern_offset=" do
+
+        should "set the offset" do
+          assert_not_equal 5, @arpeggiator.pattern_offset
+          @arpeggiator.pattern_offset = 5
+          assert_equal 5, @arpeggiator.pattern_offset
+        end
+
+      end
+
+    end
+
+  end
+
+end
--- test/arpeggiator_test.rb
+++ test/arpeggiator_test.rb
@@ -1,84 +1,34 @@
 require "helper"
 
 class Diamond::ApeggiatorTest < Test::Unit::TestCase
-    
-  def test_omni_on
-    arp = Diamond::Arpeggiator.new(:rx_channel => 3)
-    message = MIDIMessage::NoteOn["C4"].new(10, 100)
-    arp.add(message)
-    assert_empty arp.sequence.instance_variable_get("@input_queue")
 
-    message2 = MIDIMessage::NoteOn["C4"].new(3, 100)
-    arp.add(message2)
-    arp.sequence.instance_variable_get("@input_queue").expects(:concat).once.with([message2])
+  context "Arpeggiator" do
 
-    arp.omni_on
-    message3 = MIDIMessage::NoteOn["C4"].new(2, 100)
-    arp.add(message3)
-    arp.sequence.instance_variable_get("@input_queue").expects(:concat).once.with([message3])       
-  end
-  
-  def test_set_rate
-    a = Diamond::Arpeggiator.new(:rate => 8)
-    a.rate = 16
-    assert_equal(16, a.rate)
-  end
-  
-  def test_set_range
-    a = Diamond::Arpeggiator.new(:range => 4)
-    a.range += 1
-    assert_equal(5, a.range)    
-  end
-  
-  def test_set_interval
-    a = Diamond::Arpeggiator.new(:interval => 7)
-    a.interval = 12
-    assert_equal(12, a.interval)    
-  end
-  
-  def test_set_gate
-    a = Diamond::Arpeggiator.new(:gate => 75)
-    a.gate = 125
-    assert_equal(125, a.gate)    
-  end
-  
-  def test_set_pattern_offset
-    a = Diamond::Arpeggiator.new(:pattern_offset => 1)
-    a.pattern_offset = 5
-    assert_equal(5, a.pattern_offset)    
-  end
-  
-  def test_pass_in_source
-    input = $test_device[:input]
-    arp = Diamond::Arpeggiator.new(:midi => input)
-    assert_equal($test_device[:input], arp.midi_sources.first)     
-  end
-      
-  def test_add_source
-    input = $test_device[:input]
-    arp = Diamond::Arpeggiator.new
-    arp.add_midi_source(input)
-    assert_equal($test_device[:input], arp.midi_sources.first)     
+    context "#initialize" do
+
+      should "have defaults" do
+        @arpeggiator = Diamond::Arpeggiator.new
+        assert_equal 8, @arpeggiator.rate
+        assert_equal 3, @arpeggiator.range    
+        assert_equal 12, @arpeggiator.interval  
+      end
+
+      should "allow setting params" do
+        @arpeggiator = Diamond::Arpeggiator.new(:interval => 7, :range => 4, :rate => 16)
+        assert_equal 16, @arpeggiator.rate
+        assert_equal 4, @arpeggiator.range    
+        assert_equal 7, @arpeggiator.interval  
+      end
+
+      should "allow passing in input" do
+        @input = $test_device[:input]
+        @arpeggiator = Diamond::Arpeggiator.new(:midi => @input)
+        assert_not_empty @arpeggiator.midi_sources
+        assert @arpeggiator.midi_sources.include?(@input) 
+      end
+
+    end
+
   end
 
-  def test_add_remove_source
-    input = $test_device[:input]
-    arp = Diamond::Arpeggiator.new
-    arp.add_midi_source(input)
-    assert_equal($test_device[:input], arp.midi_sources.first)
-    arp.remove_midi_source(input)
-    assert_equal(nil, arp.midi_sources.first)       
-  end
-  
-  def test_mute
-    arp = Diamond::Arpeggiator.new 
-    assert_equal(false, arp.muted?)       
-    arp.mute = true
-    assert_equal(true, arp.muted?)
-    arp.mute = false
-    assert_equal(false, arp.muted?)
-    arp.toggle_mute
-    assert_equal(true, arp.muted?)
-  end
-  
 end
