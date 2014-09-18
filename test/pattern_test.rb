4a8e90e60f955fafa1ac44d217ef64a4292a4a6d
Ari Russo <ari.russo@gmail.com>
2014-09-18 14:08:19 -0700

    working pattern test

--- test/pattern_test.rb
+++ test/pattern_test.rb
@@ -1,51 +1,63 @@
-#!/usr/bin/env ruby
-
-require 'helper'
+require "helper"
 
 class PatternTest < Test::Unit::TestCase
 
-  include Diamond
-  include TestHelper
-  
-  def test_new_pattern
-    pattern = Pattern.new("test") do |r, i|
-      a = []
-      0.upto(r) { |n| a << (n * i) }
-      a
+  context "Pattern" do
+
+    setup do
+      ::OSC::EMServer.any_instance.stubs(:run).returns(:true)
     end
-    result = pattern.compute(2, 12)
-    assert_equal([0, 12, 24], result)
-  end
-  
-  def test_range_and_interval
-    pattern = Pattern.find("Up")
-    result = pattern.compute(3, 7)
-    assert_equal(4, result.length)   
-    assert_equal([0, 7, 14, 21], result)            
-  end
-    
-  def test_up
-    pattern = Pattern.find("Up")
-    result = pattern.compute(2, 12)
-    assert_equal([0, 12, 24], result)    
-  end
-  
-  def test_down
-    pattern = Pattern.find("Down")
-    result = pattern.compute(2, 12)
-    assert_equal([24, 12, 0], result)    
+
+    context "#initialize" do
+
+      should "create usable pattern" do
+        pattern = Diamond::Pattern.new("test") do |range, interval|
+          0.upto(range).map { |num| num * interval }
+        end
+        result = pattern.compute(2, 12)
+        assert_equal [0, 12, 24], result
+      end
+
+    end
+
+    context "#compute" do
+
+      should "reflect range and interval" do
+        pattern = Diamond::Pattern.find("Up")
+        result = pattern.compute(3, 7)
+        assert_equal 4, result.length
+        assert_equal [0, 7, 14, 21], result        
+      end
+
+    end
+    context "Presets" do
+
+      should "populate Up" do
+        pattern = Diamond::Pattern.find("Up")
+        result = pattern.compute(2, 12)
+        assert_equal [0, 12, 24], result    
+      end
+
+      should "populate Down" do
+        pattern = Diamond::Pattern.find("Down")
+        result = pattern.compute(2, 12)
+        assert_equal [24, 12, 0], result  
+      end
+
+      should "populate UpDown" do
+        pattern = Diamond::Pattern.find("UpDown")
+        result = pattern.compute(3, 12)
+        assert_equal [0, 12, 24, 36, 24, 12, 0], result   
+      end
+
+      should "populate DownUp" do
+        pattern = Diamond::Pattern.find("DownUp")
+        result = pattern.compute(3, 12)
+        assert_equal [36, 24, 12, 0, 12, 24, 36], result   
+      end
+
+    end
+
   end
 
-  def test_updown
-    pattern = Pattern.find("UpDown")
-    result = pattern.compute(3, 12)
-    assert_equal([0, 12, 24, 36, 24, 12, 0], result)    
-  end
-
-  def test_downup
-    pattern = Pattern.find("DownUp")
-    result = pattern.compute(3, 12)
-    assert_equal([36, 24, 12, 0, 12, 24, 36], result)    
-  end
-      
 end
