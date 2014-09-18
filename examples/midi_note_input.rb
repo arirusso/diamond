c0b46297457140868a6b1595bb71224207cac1d9
Ari Russo <ari.russo@gmail.com>
2014-09-18 15:36:27 -0700

    working example

--- examples/midi_note_input.rb
+++ examples/midi_note_input.rb
@@ -1,47 +1,48 @@
 #!/usr/bin/env ruby
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
-#
-# this example shows how to enter notes using MIDI input
-#
+# Enter notes using MIDI input
+
 
 require "diamond"
 
-#
-# you will want to select the MIDI input that your controller or other device is connected to
+# Select the MIDI input that your controller or other device is connected to
 #
 # here is an example that explains a bit more about selecting devices with unimidi:
 # http://github.com/arirusso/unimidi/blob/master/examples/select_a_device.rb
-#
 
 @input = UniMIDI::Input.gets
 @output = UniMIDI::Output.gets
 
-opts = { 
+options = { 
   :gate => 20,   
   :interval => 7,
   :midi => [@output, @input],
-  :pattern => Diamond::Pattern["UpDown"],
+  #:midi_debug => true, # uncomment this for debug output about MIDI messages in the console
+  :pattern => "UpDown",
   :range => 4, 
   :rate => 8
 }
 
-arp = Diamond::Arpeggiator.new(105, opts)
+@clock = Diamond::Clock.new(101)
+@arpeggiator = Diamond::Arpeggiator.new(options)
+@clock << @arpeggiator
 
-# normally by default the arpeggiator will be in "omni mode" or in other words, accept notes
-# from all MIDI channels
+# By default the arpeggiator will be in "omni mode", reacting to notes received from all MIDI channels
 
-# to tell the arpeggiator to only look at a single channel, just set a channel
+# To only look at a single channel, set the input channel via Arpeggiator#rx_channel=
+# Can also be passed in to the Arpeggiator constructor via the :rx_channel option
 
-# can also be passed in via the :rx_channel option
+@arpeggiator.rx_channel = 0 
 
-arp.rx_channel = 0 
-
-# you can also call arp.rx_channel = nil or arp.omni_on to return it to omni mode
-
+# You can then call arp.rx_channel = nil or arp.omni_on to return it to omni mode
 # (Diamond does not respond to MIDI Omni On/Off messages)
-   
-arp.start(:focus => true)
 
-# now when you play notes in to the input, they will be sent to the arpeggiator the same way
-# we used arp.add(chord in the simple example)
+# It's also possible to set the channel that the arpeggiator will output on
+
+@arpeggiator.tx_channel = 1
+
+@clock.start(:focus => true)
+
+# When you play notes, they will be sent to the arpeggiator the same way 
+# Arpeggiator#add is used in most of the other examples
