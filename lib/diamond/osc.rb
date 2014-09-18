850cdd4e3c0f0db56a4c0e75899909853a527a65
Ari Russo <ari.russo@gmail.com>
2014-09-18 14:07:42 -0700

    use osc patch for granular event machine usage

--- lib/diamond/osc.rb
+++ lib/diamond/osc.rb
@@ -42,9 +42,10 @@ module Diamond
       # Start the server
       # @return [Thread]
       def start_server
-        @thread = Thread.new do 
+        @thread = Thread.new do
           begin
-            @server.run
+            EM.epoll
+            EM.run { @server.run }
           rescue Exception => exception
             Thread.main.raise(exception)
           end
@@ -60,5 +61,42 @@ module Diamond
       Node.new(*args)
     end
 
+  end
+end
+
+# Patch the OSC module
+#
+module OSC
+  class EMServer
+
+    def run
+      open
+    end
+
+    def open
+      EM::open_datagram_socket("0.0.0.0", @port, Connection)
+    end
+
+  end
+end
+
+module EventMachine
+  module WebSocket
+    def self.start(options, &blk)
+      #EM.epoll
+      #EM.run {
+        trap("TERM") { stop }
+        trap("INT")  { stop }
+
+        run(options, &blk)
+      #}
+    end
+
+    def self.run(options)
+      host, port = options.values_at(:host, :port)
+      EM.start_server(host, port, Connection, options) do |c|
+        yield c
+      end
+    end
   end
 end
