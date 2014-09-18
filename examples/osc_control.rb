d13705363932471877451f3cbc40c0deb5082541
Ari Russo <ari.russo@gmail.com>
2014-09-18 01:02:53 -0700

    formatting

--- examples/osc_control.rb
+++ examples/osc_control.rb
@@ -1,16 +1,22 @@
 #!/usr/bin/env ruby
 $:.unshift File.join( File.dirname( __FILE__ ), '../lib')
 
-# A simple OSC control setup
-# You'll need the osc-ruby gem which doesn't bundle with diamond by default
+# Use OSC to control the arpeggiator
 
 require "diamond"
 
 @output = UniMIDI::Output.gets
 
 osc_map = [
-  { :property => :interval, :address => "/1/rotaryA", :value => (0..1.0) },
-  { :property => :transpose, :address => "/1/rotaryB" }
+  { 
+    :property => :interval, 
+    :address => "/1/rotaryA", 
+    :value => (0..1.0) # value is optional, defaults to 0..1.0
+  },
+  { 
+    :property => :transpose, 
+    :address => "/1/rotaryB" 
+  }
 ]
 
 options = { 
@@ -19,20 +25,16 @@ options = {
   :midi => @output,
   :osc_map => osc_map,
   :osc_port => 8000,
-  :pattern => Diamond::Pattern["UpDown"],
+  :pattern => "UpDown",
   :range => 4, 
   :rate => 8,
   :resolution => 128
 }
 
-
 @arpeggiator = Diamond::Arpeggiator.new(options)
-
 @clock = Diamond::Clock.new(110)
 @clock << @arpeggiator
 
 chord = ["C1", "G1", "Bb2", "A3"]
-
 @arpeggiator.add(*chord)
-   
 @clock.start(:focus => true)
